0252ce0533b0467d8115cecaf3fdd05371995dcc
NazarK0 <vanivskyi.dev@gmail.com>
2025-08-25 21:37:55 +0300

    create positioning embeddings

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import torch
 from src.gpt_dataset import create_dataloader_v1
 
 with open("./data/the-verdict.txt", "r", encoding="utf-8") as file:
@@ -21,4 +22,29 @@ dataloader = create_dataloader_v1(raw_text, batch_size=CONTEXT_SIZE * 2, max_len
 data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
 print("Inputs:\n", inputs)
-print("Targets:\n", targets)
+# print("Targets:\n", targets)
+print("\nInputs shape:\n", inputs.shape)
+# print("Targets shape:", targets.shape)
+
+# Create token embeddings
+input_ids = torch.tensor([2, 3, 5, 7])
+vocabulary_size = 50257
+output_dimensions = 256
+
+torch.manual_seed(123)
+token_embedding_layer = torch.nn.Embedding(vocabulary_size, output_dimensions)
+print(token_embedding_layer.weight)
+
+token_embeddings = token_embedding_layer(inputs)
+
+# test
+print("\nToken Embeddings:\n", token_embeddings)
+print("\nToken Embeddings shape:\n", token_embeddings.shape)
+
+# Create positional embeddings
+positional_embedding_layer = torch.nn.Embedding(CONTEXT_SIZE, output_dimensions)
+positional_embeddings = positional_embedding_layer(torch.arange(CONTEXT_SIZE))
+
+# test
+print("\nPositional Embeddings:\n", positional_embeddings)
+print("\nPositional Embeddings shape:\n", positional_embeddings.shape)
